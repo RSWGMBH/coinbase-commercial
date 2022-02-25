fb27852bfe9ad56510c46367686a31804e67cd1d
RSWGMBH <100408944+RSWGMBH@users.noreply.github.com>
2022-02-25 15:22:32 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import QRCode from "react-qr-code";
 import styles from "../styles/Home.module.css";
 
-const size = 200;
+const size = 400;
 const colors = ["#AD1EC2", "#D36EA6", "#D0BDD1", "#83BD16", "#16BDB1"];
 
 export default function Home() {
